625b52ae10f4845c0980850a6f9a0fb014ca9a8e
Xiaochun Shen <snowdonshen@gmail.com>
2011-11-13 13:10:29 +0800

    add function about remove answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,4 +4,12 @@ class AnswersController < ApplicationController
     @answer = @question.answers.create(params[:answer])
     redirect_to question_path(@question)
   end
+  
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    @answer.destroy
+    redirect_to question_path(@question)
+  end
+  
 end
